2c848b06727e6887f6baaf65ee3e792a5153c9da
xiaojian <jian.xiao@thoughtworks.com>
2025-10-21 09:40:31 +0800

    [392] is subsequence

---
+++ src/main/java/leetcode/editor/cn/IsSubsequence.java
@@ -0,0 +1,77 @@
+/**
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * <p>
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而
+ * "aec"不是）。
+ * <p>
+ * 进阶：
+ * <p>
+ * 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+ * <p>
+ * <p>
+ * 致谢：
+ * <p>
+ * 特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：s = "abc", t = "ahbgdc"
+ * 输出：true
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：s = "axc", t = "ahbgdc"
+ * 输出：false
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 0 <= s.length <= 100
+ * 0 <= t.length <= 10^4
+ * 两个字符串都只由小写字符组成。
+ * <p>
+ * <p>
+ * Related Topics 双指针 字符串 动态规划 👍 1201 👎 0
+ */
+
+package leetcode.editor.cn;
+
+public class IsSubsequence {
+    public static void main(String[] args) {
+        Solution solution = new IsSubsequence().new Solution();
+        System.out.println(solution.isSubsequence("axc", "ahbgdc"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            if (s.length() > t.length()) {
+                return false;
+            }
+
+            int i = 0;
+            int j = 0;
+
+            while (i < s.length() && j < t.length()) {
+                while (j < t.length() && s.charAt(i) != t.charAt(j)) {
+                    j++;
+                }
+                if(j < t.length()){
+                    i++;
+                    j++;
+                }
+            }
+            return i == s.length();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
